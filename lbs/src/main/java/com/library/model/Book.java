0e5993e3266693d70395957d8ab00f5915c9e5f7
Alan <alanronimathew@gmail.com>
2026-05-14 11:29:15 +0400

    2 Entities created

--- lbs/src/main/java/com/library/model/Book.java
+++ lbs/src/main/java/com/library/model/Book.java
@@ -1,5 +1,66 @@
 package com.library.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Books")
 public class Book {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long bid;
+    String name;
+    String author;
+    boolean borrowed = false;
+
+    public Book() {
+    }
+
+    public Long getBid() {
+        return bid;
+    }
+
+    public void setBid(Long bid) {
+        this.bid = bid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        this.borrowed = borrowed;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Book{");
+        sb.append("bid=").append(bid);
+        sb.append(", name=").append(name);
+        sb.append(", author=").append(author);
+        sb.append(", borrowed=").append(borrowed);
+        sb.append('}');
+        return sb.toString();
+    }
 }
--- lbs/src/main/java/com/library/model/Member.java
+++ lbs/src/main/java/com/library/model/Member.java
@@ -1,5 +1,59 @@
 package com.library.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Members")
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long mid;
+    String name;
+    String phone;
+    boolean hasMembership = false;
+    int booksTaken = 0;
+    
+    public Long getMid() {
+        return mid;
+    }
+    public void setMid(Long mid) {
+        this.mid = mid;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public boolean isHasMembership() {
+        return hasMembership;
+    }
+    public void setHasMembership(boolean hasMembership) {
+        this.hasMembership = hasMembership;
+    }
+    public int getBooksTaken() {
+        return booksTaken;
+    }
+    public void setBooksTaken(int booksTaken) {
+        this.booksTaken = booksTaken;
+    }
+    public Member() {
+    }
+    @Override
+    public String toString() {
+        return "Member [mid=" + mid + ", name=" + name + ", phone=" + phone + ", hasMembership=" + hasMembership
+                + ", booksTaken=" + booksTaken + "]";
+    }
+
     
 }
